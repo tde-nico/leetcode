97ea402df240e4098200b40c080560da4fb5f70d
tde-nico <tommyden5@gmail.com>
2025-08-21 23:43:49 +0200

    898

---
+++ 898.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        result_ors = set()
+        current_ors = set()
+
+        for x in arr:
+            next_ors = {x | y for y in current_ors}
+            next_ors.add(x)
+            result_ors.update(next_ors)
+            current_ors = next_ors
+            
+        return len(result_ors)

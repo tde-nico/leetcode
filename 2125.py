9d4525296c29a3dc2c4a5ab628c5e1df6795e5ae
tde-nico <tommyden5@gmail.com>
2026-03-14 17:32:15 +0100

    2125

---
+++ 2125.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        prev, ans = 0, 0
+        
+        for s in bank:
+            count = 0
+            for c in s:
+                if c == '1':
+                    count += 1
+            if count != 0:
+                ans += (prev * count)
+                prev = count
+        
+        return ans
